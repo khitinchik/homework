3ccdf76039fa276849e9c2866cd4b51116944660
khitinchik <3Mimosa12>
2021-12-18 19:06:08 +0400

    добавили новые методы

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,9 +1,35 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class HomeWork {
 
+
+    @BeforeAll
+    static void beforeAll () {
+        System.out.println("@BeforeAll method!");
+    }
+
+    @BeforeEach
+    void beforeEach () {
+        System.out.println("    @BeforeEach method!");
+    }
+
+    @AfterEach
+    void afterEach () {
+        System.out.println("    @AfterEach method!");
+    }
+
+    @AfterAll
+    static void afterAll () {
+        System.out.println("@AfterAll method!");
+    }
+
     @Test
     void work () {
+        System.out.println("w");
+    }
+
+    @Test
+    void workOne () {
         System.out.println("w");
     }
 
