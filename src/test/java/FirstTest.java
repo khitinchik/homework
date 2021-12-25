968a2ca1b6ab92c95a69e443ef717530a0fe46ff
khitinchik <3Mimosa12>
2021-12-26 01:06:08 +0400

    testnew

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,22 +1,29 @@
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 public class FirstTest {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1366x768";
+    }
         @Test
         void test () {
             open("https://demoqa.com/automation-practice-form");
 
-            $("#firstName").setValue("name");
+            $("#firstName").setValue("nastya");
             $("#lastName").setValue("surname");
-            $("#userEmail").setValue("email");
+            $("#userEmail").setValue("nasok97@mail.ru");
             $("#genterWrapper").$(byText("Female")).click();
-            $("#userNumber").setValue("89277047002");
+            $("#userNumber").setValue("9277047002");
             $("#dateOfBirthInput").click();
             $(".react-datepicker__month-select").selectOption("December");
             $(".react-datepicker__year-select").selectOption("2000");
@@ -24,6 +31,12 @@ public class FirstTest {
             $("#subjectsInput").setValue("English").pressEnter();
             $("#hobbiesWrapper").$(byText("Sports")).click();
             $("#uploadPicture").uploadFile(new File("src/test/resources/1.png"));
+            $("#currentAddress").setValue("address");
+            $("#react-select-3-input").setValue("NCR").pressEnter();
+            $("#react-select-4-input").setValue("Delhi").pressEnter();
+            $("#submit").scrollTo().click();
 
+            sleep(17000);
+            $(".table-responsive").shouldHave(text("nastya"));
         }
     }
